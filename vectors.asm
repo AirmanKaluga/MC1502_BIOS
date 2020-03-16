c6eec395bf5200ef0292b52047fc3d2fd42f29d7
Airman <34879583+AirmanKaluga@users.noreply.github.com>
2020-03-16 17:17:00 +0300

    Таки откатываем. Под Windows 3.0 не рабоатет клавиатура. Можно конечно написать драйвер, но кто это будет делать.

--- vectors.asm
+++ vectors.asm
@@ -13,15 +13,27 @@ int_vec_table_1:
                 dw offset int_10h         ; Offset int_10h
                 dw offset int_11h         ; Ofsset int_11h
                 dw offset int_12h         ; Offset int_12h
-                dw offset int_13h	  ; Offset int_13h
+                dw offset int_13h	        ; Offset int_13h
                 dw offset int_14h         ; Offset int_14h
                 dw offset dummy_int       ; Offset int_15h
                 dw offset int_16h         ; Offset int_16h
                 dw offset int_17h         ; Offset int_17h
                 dw offset dummy_int       ; Offset int_18h
                 dw offset int_19h         ; Offset int_19h
-                dw offset int_1Ah	  ; Offset int_1Ah
+                dw offset int_1Ah	        ; Offset int_1Ah
                 dw offset dummy_int       ; Offset int_1Bh
                 dw offset dummy_int       ; Offset int_1Ch
-                dw offset int_1Dh	  ; Offset int_1Dh
+                dw offset int_1Dh	        ; Offset int_1Dh
                 dw offset int_1Eh         ; Offset int_1Eh
+                
+int_vec_table_2:
+                dw offset int_68h
+                dw offset int_69h
+                dw offset int_6Ah
+                dw offset int_6Bh
+                dw offset int_6Ch
+                dw offset int_6Dh
+                dw offset int_6Eh
+                dw offset int_6Fh
+                
+                
